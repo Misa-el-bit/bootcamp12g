b2408670390cab821f415b3598758e07ee52aecc
Misa-El <mmancillaglz07@gmail.com>
2021-06-15 22:50:03 -0500

    s1javascript

---
+++ s9javascript/js/main.js
@@ -0,0 +1,30 @@
+/*window.alert("Hola koders")
+console.log("Hola coders desde consola")
+console.error("Houston tenemos un problema")
+console.warn("Cuidado")
+*/
+
+/*prompt("Ingresa tu nombre")*/
+
+/*var firstNumber = prompt("Ingresa un numero")
+var secondNumber = prompt("Ahora ingresa otro numero")*/
+
+/*parseInt(secondNomber)*/
+var firstNumber = parseInt(prompt("Ingresa un numero"))
+var secondNumber = parseInt(prompt("Ahora ingresa otro numero"))
+
+console.log(firstNumber + secondNumber)
+console.log(firstNumber - secondNumber)
+console.log(firstNumber * secondNumber)
+console.log(firstNumber / secondNumber)
+console.log(firstNumber % secondNumber)
+
+var firstNumber = prompt("ingresa un numero")
+var secondNumber = prompt("Ingresa otro numero")
+
+console.log(firstNumber>secondNumber)
+
+var firstLetter = prompt("ingresa una letra")
+var secondLetter = prompt("Ingresa otra letra")
+
+console.log(firstLetter > secondLetter)
